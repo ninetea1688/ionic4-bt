ddb08a739c6f269d72970575b94c331165fa3500
cybernude <ninetea@gmail.com>
2020-02-03 20:51:14 +0700

    commit all code first time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BluetoothLE } from '@ionic-native/bluetooth-le/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    HttpClientModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+    BluetoothLE,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { BluetoothLE } from '@ionic-native/bluetooth-le/ngx';
+import { Platform } from '@ionic/angular';
+import { map } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +12,28 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(
+    public bluetoothle: BluetoothLE, public plt: Platform
+  ) {
+
+
+    this.plt.ready().then((readySource) => {
+
+      console.log('Platform ready from', readySource);
+
+      this.bluetoothle.initialize().subscribe(ble => {
+        console.log('ble', ble.status) // logs 'enabled'
+        console.log('this bt data : ', ble)
+      });
+
+      // this.http.get('https://randomuser.me/api/, {params: params})
+      // .subscribe(data => {
+      //   this.data = data.results;
+      //   resolve(this.data);
+      // }));
+
+    });
+
+  }
 
 }
